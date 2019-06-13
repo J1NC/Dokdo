80bf73b66b0d3cd32967d8a6d4d86429c715a341
J1NC <womin1120@naver.com>
2019-06-13 10:51:52 +0900

    [ADD] Introduction UI 및 각각의 세부 Activity 연결

--- app/src/main/java/kr/hs/dgsw/jin_um/dokdo/IntroductionActivity.java
+++ app/src/main/java/kr/hs/dgsw/jin_um/dokdo/IntroductionActivity.java
@@ -1,7 +1,9 @@
 package kr.hs.dgsw.jin_um.dokdo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class IntroductionActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class IntroductionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_introduction);
+    }
+
+    public void changeTabIntroduction(View v){
+        Intent intent = new Intent();
+
+        switch (v.getId()){
+            case R.id.cardViewIntroductionPlace : intent =  new Intent(this, IntroductionPlaceActivity.class);
+            break;
+            case R.id.cardViewIntroductionView : intent = new Intent(this, IntroductionViewActivity.class);
+            break;
+        }
+
+        startActivity(intent);
     }
 }
