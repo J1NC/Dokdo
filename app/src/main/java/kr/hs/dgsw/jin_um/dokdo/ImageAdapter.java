717df3c33dee42023c821a710989705e97bd83c9
J1NC <womin1120@naver.com>
2019-06-16 20:57:42 +0900

    [ADD] IntroductionPlaceActivity 완료

---
+++ app/src/main/java/kr/hs/dgsw/jin_um/dokdo/ImageAdapter.java
@@ -0,0 +1,55 @@
+package kr.hs.dgsw.jin_um.dokdo;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ImageAdapter extends PagerAdapter {
+    private List<Integer> images;
+    private List<String> imagesDescription;
+    private LayoutInflater layoutInflater;
+    private Context context;
+
+    @Override
+    public int getCount() {
+        return images.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return view == ((ConstraintLayout) o);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = layoutInflater.inflate(R.layout.slider, container, false);
+        ImageView imageView = (ImageView) v.findViewById(R.id.imageViewSlider);
+        TextView textView = (TextView) v.findViewById(R.id.textViewDescriptSlider);
+        imageView.setImageResource(images.get(position));
+        textView.setText(imagesDescription.get(position));
+        container.addView(v);
+        return v;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object){
+        container.invalidate();
+    }
+
+    public ImageAdapter(Context context, List<Integer> images, List<String> imagesDescription){
+        this.context = context;
+        this.images = images;
+        this.imagesDescription = imagesDescription;
+    }
+}
--- app/src/main/java/kr/hs/dgsw/jin_um/dokdo/IntroductionPlaceActivity.java
+++ app/src/main/java/kr/hs/dgsw/jin_um/dokdo/IntroductionPlaceActivity.java
@@ -1,8 +1,11 @@
 package kr.hs.dgsw.jin_um.dokdo;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class IntroductionPlaceActivity extends AppCompatActivity {
     private final int DEPTH = 1;
@@ -10,6 +13,11 @@ public class IntroductionPlaceActivity extends AppCompatActivity {
     private DokdoHelper dokdoHelper;
     private TextView textViewExplanation;
     private String explanation;
+    private ImageAdapter imageAdapter;
+    private ViewPager viewPager;
+    private List<Integer> images;
+    private List<String> imagesDescription;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +30,11 @@ public class IntroductionPlaceActivity extends AppCompatActivity {
     private void init(){
         textViewExplanation = findViewById(R.id.textViewExIntroductionPlace);
         explanation = dokdoHelper.getExplanation(depth1, DEPTH);
-
+        images = dokdoHelper.getImages(depth1, DEPTH);
+        imagesDescription = dokdoHelper.getImageExplanation(depth1, DEPTH);
+        viewPager = (ViewPager) findViewById(R.id.imageSliderIntroductionPlace);
+        imageAdapter = new ImageAdapter(this, images, imagesDescription);
+        viewPager.setAdapter(imageAdapter);
         textViewExplanation.setText(explanation);
     }
 
